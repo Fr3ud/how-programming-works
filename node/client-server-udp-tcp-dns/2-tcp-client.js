cec2f72f7d12c0a0f5ccf85b296470e9fd5df6ca
Fr3ud <oo.pavelko@gmail.com>
2021-10-09 23:40:47 +0300

    add tcp client

---
+++ node/client-server-udp-tcp-dns/2-tcp-client.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const net = require('net');
+
+const socket = new net.Socket();
+
+socket.on('data', data => {
+    console.log('🏠 ' + data);
+});
+
+socket.connect({
+    port: 8848,
+    host: '127.0.0.1',
+}, () => socket.write('💚'));
