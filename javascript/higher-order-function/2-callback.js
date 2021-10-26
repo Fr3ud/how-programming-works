95e41dd1b96dde570f0db07adc35102c7f237dd0
Fr3ud <oo.pavelko@gmail.com>
2021-10-26 22:29:36 +0300

    add callback

---
+++ javascript/higher-order-function/2-callback.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const fn = (par, callback) => {
+  if (!par) {
+    callback(new Error('Parameter needed'));
+
+    return;
+  }
+
+  callback(null, `Data ${par}`);
+
+  return 'Value';
+};
+
+const res = fn('example', (err, data) => {
+  if (err) throw err;
+  
+  console.dir({ data });
+});
+
+console.dir({ res });
